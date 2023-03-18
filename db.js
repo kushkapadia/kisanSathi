cfe687a32f46dd9d60e3aa8d1f6048e1116cee25
FameILY <bhambidsatyam@gmail.com>
2023-03-18 19:55:54 +0530

    againsry

--- db.js
+++ db.js
@@ -2,6 +2,6 @@ document.write("Hi from satyam!");
 document.write("Hi from Kush!");
 document.write("Hello From Harshit");
 
-document.write("satyamhere!");
+document.write("satyamhere! again!");
 
 document.write("Quasar Hackathon Event 2023");
