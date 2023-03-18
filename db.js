4925804ec20da915a6e9dcf8b075c469c42ac856
Harshit Chauhan <98049847+proxxyharshit@users.noreply.github.com>
2023-03-18 21:00:56 +0530

    test

--- db.js
+++ db.js
@@ -1,9 +1,1 @@
-document.write("Hi from satyam!");
-document.write("Hi from Kush!");
-document.write("Hello From Harshit");
-
-document.write("satyamhere! again! yes");
-
-document.write("Quasar Hackathon Event 2023");
-
-document.write("fetchthis");
+document.write("Hello Team");
